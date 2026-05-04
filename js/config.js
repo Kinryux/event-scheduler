9f74665ae4b62b2d8f3ea456148c65152fd13a65
Kinryux <kinryu.mmi@gmail.com>
2026-05-04 15:15:50 -0700

    Update WEB_APP_URL to the deployed URL

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Replace WEB_APP_URL with the deployment URL from Phase 5.
 window.APP_CONFIG = {
-  WEB_APP_URL: 'PASTE_DEPLOYED_WEB_APP_URL_HERE'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw5-MBIBW5GllugqTLfdXoOjS19ksbP2l1HGCc5Dzwl4CL5m0ohNEVBJpGUg42b_770Wg/exec'
 };
