50147f647493eba54828071ea7f06c1dcf62c964
Kinryux <kinryu.mmi@gmail.com>
2026-05-04 15:40:22 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Replace WEB_APP_URL with the deployment URL from Phase 5.
 window.APP_CONFIG = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw5-MBIBW5GllugqTLfdXoOjS19ksbP2l1HGCc5Dzwl4CL5m0ohNEVBJpGUg42b_770Wg/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxHlylLFWYxN2xrPl_FcGIWkc7NnQcWlqQXhr2q6BYp1pihHZ1CjMaVodliqqWR12smdg/exec'
 };
